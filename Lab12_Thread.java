6401e8f7e71a9e18c90e87b3701cfa010fb62b1c
Vigneshsmvitm <vignesh.23cs026@sode-edu.in>
2024-12-10 19:29:11 +0530

    Add files via upload

---
+++ Lab12_Thread.java
@@ -0,0 +1,45 @@
+class MyThread extends Thread
+{
+    public MyThread(String name)
+    {
+        super(name);
+        start();
+
+    }
+    public void run()
+    {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Child Thread"+i);
+            try
+            {
+                Thread.sleep(500);
+            }
+            catch(InterruptedException e)
+            {
+                System.out.println("Child Thread is interrupted");
+            }
+        }
+    }
+}
+public class Lab12_Thread
+{
+    public static void main(String args[])
+    {
+        System.out.println("Thread");
+        MyThread t1=new MyThread("ChildThread");
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Main Thread "+i);
+            try
+            {
+                Thread.sleep(500);
+            }
+            catch(InterruptedException e)
+            {
+                System.out.println("Main Thread is interrupted");
+            }
+        }
+    }
+
+}
